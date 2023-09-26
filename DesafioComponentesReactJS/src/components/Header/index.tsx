061885790ad580f3885a43efc85d52dc8f2083c7
walyson <walysonmoises1342@gmail.com>
2023-09-26 15:01:28 -0300

    header

---
+++ DesafioComponentesReactJS/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css'
+const Header = () => {
+  return (
+    <header>
+        <h1>DSCarrosTop</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ DesafioComponentesReactJS/src/routes/LandingPage/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+const LandingPage = () => {
+  return (
+    <>
+    <Header></Header>
+    <main>
+        <section></section>
+    </main>
+    </>
+  )
+}
+
+export default LandingPage
